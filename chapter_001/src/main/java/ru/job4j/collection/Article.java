12bb08eab121db3586e14a2f46f17938bf17ac0d
VadimShein <shein.v94@mail.ru>
2020-08-04 19:02:38 +0600

    1. Генератор текста

--- chapter_001/src/main/java/ru/job4j/collection/Article.java
+++ chapter_001/src/main/java/ru/job4j/collection/Article.java
@@ -1,11 +1,19 @@
 package ru.job4j.collection;
 
+import java.util.Objects;
+
 public class Article {
     public static boolean generateBy(String origin, String line) {
         boolean rsl = true;
-        String[] lineArray = line.split(" ");
-        for (String str : lineArray) {
-            if (!origin.contains(str)) {
+        java.util.HashMap<String, String> map = new java.util.HashMap<>();
+        String[] originArray = origin.replaceAll("[^a-zA-Zа-яА-Я ]", "").toLowerCase().split(" ");
+        String[] lineArray = line.replaceAll("[^a-zA-Zа-яА-Я ]", "").toLowerCase().split(" ");
+
+        for (String strOrigin : originArray) {
+            map.put(strOrigin, strOrigin);
+        }
+        for (String strLine : lineArray) {
+            if (!Objects.equals(strLine, map.get(strLine))) {
                 rsl = false;
                 break;
             }
