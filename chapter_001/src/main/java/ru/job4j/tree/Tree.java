e359a07c7c8e14a7c2c83b3469a4b41e3269d4fe
VadimShein <shein.v94@mail.ru>
2020-08-06 00:40:21 +0600

    2. Добавить метод boolean isBinary() [#303951]

--- chapter_001/src/main/java/ru/job4j/tree/Tree.java
+++ chapter_001/src/main/java/ru/job4j/tree/Tree.java
@@ -1,6 +1,7 @@
 package ru.job4j.tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 class Tree<E> implements SimpleTree<E> {
     private final Node<E> root;
@@ -20,14 +21,19 @@ class Tree<E> implements SimpleTree<E> {
         return rsl;
     }
 
+    public boolean sort(Node<E> el, Predicate<Node<E>> predicate) {
+        return predicate.test(el);
+    }
+
     @Override
     public Optional<Node<E>> findBy(E value) {
+        Predicate<Node<E>> predicate = i -> i.value.equals(value);
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.value.equals(value)) {
+            if (sort(el, predicate)) {
                 rsl = Optional.of(el);
                 break;
             }
@@ -37,14 +43,13 @@ class Tree<E> implements SimpleTree<E> {
     }
 
     public boolean isBinary() {
-        boolean rsl = false;
+        boolean rsl = true;
+        Predicate<Node<E>> predicate = i -> i.children.size() > 2;
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.children.size() <= 2) {
-                rsl = true;
-            } else {
+            if (sort(el, predicate)) {
                 rsl = false;
                 break;
             }
