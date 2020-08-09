1bb26e79d5de5862fb08270cf8c92a515315c0e8
VadimShein <shein.v94@mail.ru>
2020-08-09 18:29:07 +0600

    3.0. Тестирование IO [#303979]

--- chapter_002/src/test/java/ru/job4j/io/AnalyzeTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalyzeTest.java
@@ -1,8 +1,10 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -11,12 +13,40 @@ import static org.junit.Assert.*;
 
 public class AnalyzeTest {
     @Test
-    public void whenPairWithoutComment() throws IOException {
+    public void whenIdenticalFiles() throws IOException {
         Analyze analyze = new Analyze();
         analyze.unavailable("C://projects/job4j_design/chapter_002/src/main/java/ru/job4j/io/data/serverLog",
                 "C://projects/job4j_design/chapter_002/src/main/java/ru/job4j/io/data/unavailable.csv");
         Path actual = Path.of("C:/projects/job4j_design/chapter_002/src//main//java/ru/job4j/io/data/unavailable.csv");
         Path expected = Path.of("C:/projects//job4j_design/chapter_002/src/main/java/ru/job4j//io/data/unavailableTestFile.csv");
         assertThat(Files.mismatch(actual, expected), is(-1L));
+    }
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void whenUsedTemp() throws IOException {
+        Analyze analyze = new Analyze();
+        analyze.unavailable("C://projects/job4j_design/chapter_002/src/main/java/ru/job4j/io/data/serverLog",
+                "C://projects/job4j_design/chapter_002/src/main/java/ru/job4j/io/data/unavailable.csv");
+
+        File source = folder.newFile("unavailable.csv");
+        try (BufferedReader in = new BufferedReader(new FileReader("C:/projects/job4j_design/chapter_002/src//main//java/ru/job4j/io/data/unavailable.csv"))) {
+            String line;
+            try (PrintWriter out = new PrintWriter(source)) {
+                while ((line = in.readLine()) != null) {
+                    out.println(line);
+                }
+            }
+        }
+
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("10:57:01;10:59:01;"
+                + "11:01:02;11:02:02;"
+                + "12:00:00;12:10:00;"));
     }
 }
