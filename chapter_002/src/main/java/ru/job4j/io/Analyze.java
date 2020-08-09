b477130105082117561db1d95cb0ac16d44ffa50
VadimShein <shein.v94@mail.ru>
2020-08-09 18:25:04 +0600

    2. Анализ доступности сервера. [#303987]

--- chapter_002/src/main/java/ru/job4j/io/Analyze.java
+++ chapter_002/src/main/java/ru/job4j/io/Analyze.java
@@ -1,43 +1,45 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Analyze {
-    public void unavailable(String source, String target) {
+    public void unavailable(String source, String target) throws FileNotFoundException {
+        ArrayList<String> list = new ArrayList();
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
             String line;
-            try (PrintWriter out = new PrintWriter(new BufferedOutputStream(
-                    new FileOutputStream(target)))) {
-                String dataStart = null;
-                String dataStop = null;
-                boolean start = false;
-                while ((line = in.readLine()) != null) {
-                    String[] strArray = line.split(" ");
-                    if (!start  && (strArray[0].equals("500") || strArray[0].equals("400"))) {
-                        dataStart = strArray[1];
-                        start = true;
-                    }
-                    if (start && (strArray[0].equals("300") || strArray[0].equals("200"))) {
-                        dataStop = strArray[1];
-                        start = false;
-                    }
-                    if (dataStart != null && dataStop != null) {
-                        out.write(dataStart + ";" + dataStop + ";" + System.lineSeparator());
-                        dataStart = null;
-                        dataStop = null;
-                    }
+            String dataStart = null;
+            String dataStop = null;
+            boolean start = false;
+            while ((line = in.readLine()) != null) {
+                String[] strArray = line.split(" ");
+                if (!start  && (strArray[0].equals("500") || strArray[0].equals("400"))) {
+                    dataStart = strArray[1];
+                    start = true;
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
+                if (start && (strArray[0].equals("300") || strArray[0].equals("200"))) {
+                    dataStop = strArray[1];
+                    start = false;
+                }
+                if (dataStart != null && dataStop != null) {
+                    list.add(dataStart + ";" + dataStop + ";" + System.lineSeparator());
+                    dataStart = null;
+                    dataStop = null;
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        write(list, target);
     }
 
-    public static void main(String[] args) {
-        Analyze ana = new Analyze();
-        ana.unavailable("./chapter_002/src/main/java/ru/job4j/io/data/serverLog",
-                "./chapter_002/src/main/java/ru/job4j/io/data/unavailable.csv");
+    private void write(List<String> list, String target) throws FileNotFoundException {
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(
+                new FileOutputStream(target)))) {
+            for (String str : list) {
+                out.write(str);
+            }
+        }
     }
 }
