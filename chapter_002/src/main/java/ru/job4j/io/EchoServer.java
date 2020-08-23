8163a7bd991851d031edec32da722aa42acdceb3
VadimShein <shein.v94@mail.ru>
2020-08-23 16:54:44 +0600

    1. Бот [#303997]

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -12,13 +12,32 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    String str = in.readLine();
+                    String str;
+                    String[] strSplit;
+                    String[] paramSplit;
+                    String answer = "Hello!";
+                    while (!(str = in.readLine()).isEmpty()) {
                         System.out.println(str);
-                        if (str.contains("Bye")) {
-                            server.close();
+                        if (str.contains("/?msg")) {
+                            strSplit = str.split(" ");
+                            for (String temp : strSplit) {
+                                if (temp.contains("/?msg")) {
+                                    paramSplit = temp.split("msg=", 2);
+                                    if (paramSplit[1].equals("Bye")) {
+                                        server.close();
+                                    }
+                                    if (paramSplit[1].equals("Hello")) {
+                                        answer = "Hello, dear friend!";
+                                    } else {
+                                        answer = paramSplit[1];
+                                    }
+                                }
+                            }
                         }
+                    }
                     if (!server.isClosed()) {
                         out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        out.write((answer).getBytes());
                     }
                 }
             }
