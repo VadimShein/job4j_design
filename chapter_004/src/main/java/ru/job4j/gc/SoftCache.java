404dd6c3ecce796802e3325b63514837ceaa13ef
VadimShein <shein.v94@mail.ru>
2020-11-03 23:51:57 +0600

    Реализации кеша на SoftReference [#304035]

--- chapter_004/src/main/java/ru/job4j/gc/SoftCache.java
+++ chapter_004/src/main/java/ru/job4j/gc/SoftCache.java
@@ -2,21 +2,32 @@ package ru.job4j.gc;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.lang.ref.ReferenceQueue;
 import java.lang.ref.SoftReference;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class SoftCache<K, V> implements Cache<K, V> {
     private Map<K, SoftReference<V>> cacheMap = new ConcurrentHashMap<>();
+    private Map<SoftReference<V>, K> dCacheMap = new ConcurrentHashMap<>();
+    private ReferenceQueue<V> refQueue =  new  ReferenceQueue<>();
 
     @Override
     public V get(K key) {
+        SoftReference<V> ref;
+        while ((ref = (SoftReference<V>) refQueue.poll()) != null) {
+            System.out.println("GC is work ------------------");
+            System.out.println(ref + " " + dCacheMap.get(ref) + " " + cacheMap.get(dCacheMap.get(ref)));
+            cacheMap.remove(dCacheMap.get(ref));
+        }
         V rsl;
         if (!cacheMap.containsKey(key)) {
             V value = download(key);
-            SoftReference<V> vol = new SoftReference<>(value);
+            SoftReference<V> vol = new SoftReference<>(value, refQueue);
             cacheMap.put(key, vol);
+            dCacheMap.put(vol, key);
             rsl = value;
+            System.out.println("cache size: " + cacheMap.size());
         } else {
             rsl = cacheMap.get(key).get();
         }
@@ -40,7 +51,7 @@ public class SoftCache<K, V> implements Cache<K, V> {
 
     public static void main(String[] args) {
         Cache<String, String> sf = new SoftCache<>();
-        String rsl = sf.get("Address.txt");
-        System.out.println(rsl);
+        System.out.println(sf.get("Address.txt"));
+        System.out.println(sf.get("Address.txt"));
     }
 }
