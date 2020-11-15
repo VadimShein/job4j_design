037f397df112fa181862c78c4221810bffa50ea5
VadimShein <shein.v94@mail.ru>
2020-11-16 02:02:07 +0600

    2. Что такое TDD? [#304002]

---
+++ chapter_005/src/test/java/u/job4j/tdd/CinemaTest.java
@@ -0,0 +1,52 @@
+package u.job4j.tdd;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+import ru.job4j.tdd.*;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+public class CinemaTest {
+
+    @Test
+    public void buy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 0);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Test
+    public void find() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+    @Test
+    public void notBuyWrongSeat() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23,  0);
+        Ticket ticket = cinema.buy(account, -2, 0, date);
+        assertThat(ticket, is(String.format("Please enter validate seat again.%n")));
+    }
+
+    @Test
+    public void notBuyWrongDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2005, 10, 10, 23,  0);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(String.format("Please enter validate date again.%n")));
+    }
+}
