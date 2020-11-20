23e66b1ca56b7769624845deffca61fdb7325b0d
VadimShein <shein.v94@mail.ru>
2020-11-21 04:15:02 +0600

    3. Шаблонизатор. [#304001]

--- chapter_005/src/test/java/ru/job4j/template/GeneratorTest.java
+++ chapter_005/src/test/java/ru/job4j/template/GeneratorTest.java
@@ -19,21 +19,19 @@ public class GeneratorTest {
         assertThat(rsl, is("I am a Petr Arsentev, Who are you? "));
     }
 
-    @Test
+    @Test (expected = NullPointerException.class)
     public void generatorIncorrectKeyExceptionTest() {
         Generator generator = new TextGenerator();
         String template = "I am a ${name}, Who are ${subject}? ";
         Map<String, String> map = Map.of("surname", "Petr Arsentev", "subject", "you");
         String rsl = generator.produce(template, map);
-        assertThat(rsl, is(String.format("Please enter validate keys.%n")));
     }
 
-    @Test
+    @Test (expected = NullPointerException.class)
     public void generatorExtraKeyExceptionTest() {
         Generator generator = new TextGenerator();
         String template = "I am a ${name}, Who are ${subject}? ";
         Map<String, String> map = Map.of("name", "Petr Arsentev", "subject", "you", "comment", "check it");
         String rsl = generator.produce(template, map);
-        assertThat(rsl, is(String.format("Please delete extra keys.%n")));
     }
 }
