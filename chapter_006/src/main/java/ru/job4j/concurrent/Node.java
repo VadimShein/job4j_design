0a9269bfa23e429c6dfd390a9a9b7addd9682b56
VadimShein <shein.v94@mail.ru>
2021-01-06 13:47:36 +0600

    3. Immutable объекты [#432978]

--- chapter_006/src/main/java/ru/job4j/concurrent/Node.java
+++ chapter_006/src/main/java/ru/job4j/concurrent/Node.java
@@ -1,10 +1,10 @@
 package ru.job4j.concurrent;
 
 public class Node<T> {
-    private Node<T> next;
-    private T value;
+    private final Node<T> next;
+    private final T value;
 
-    public Node(T value, Node<T> next) {
+    public Node(final T value, final Node<T> next) {
         this.value = value;
         this.next = next;
     }
