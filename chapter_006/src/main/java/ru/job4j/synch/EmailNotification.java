9e47e2b973f64f550773481f0dae472a815fcdb4
VadimShein <shein.v94@mail.ru>
2021-02-04 13:43:18 +0600

    2. ExecutorService рассылка почты. [#432958]

---
+++ chapter_006/src/main/java/ru/job4j/synch/EmailNotification.java
@@ -0,0 +1,54 @@
+package ru.job4j.synch;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+
+    private final ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
+
+    public void emailTo(User user) {
+        String subject = "Notification {" + user.username + "} to email {" + user.email + "}";
+        String body = "Add a new event to {" + user.username + "}";
+        pool.submit(new Runnable() {
+            @Override
+            public void run() {
+                send(subject, body, user.email);
+            }
+        });
+    }
+
+    public void send(String subject, String body, String email) {
+        System.out.println(subject + "\n" + body);
+    }
+
+    public void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class User {
+        String username;
+        String email;
+
+        public User(String username, String email) {
+            this.username = username;
+            this.email = email;
+        }
+    }
+
+    public static void main(String[] args) {
+        EmailNotification em = new EmailNotification();
+        em.emailTo(new User("Ivan", "iv.@mail.ru"));
+        em.emailTo(new User("Vasily", "vas.@mail.ru"));
+        em.close();
+    }
+}
